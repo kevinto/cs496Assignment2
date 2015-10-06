b95613009b4743196ab7d9fc9d573bb572747401
Kevin To <kevinto1989@gmail.com>
2015-10-06 01:07:03 +0000

    added 5 input methods to the edit page

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 var timeHelper = require('../helpers/timehelper.js');
 
+var title = 'CS496 Assignment 2';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var dt = timeHelper.getCurrentServerDateTime();
-  res.render('index', { title: 'CS496 Assignment 2', displayDateTime: dt });
+  res.render('index', { title: title, displayDateTime: dt });
 });
 
 module.exports = router;
